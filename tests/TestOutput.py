063726fb2979ed8a9530a53a8ba243bd08d3ce13
AdamBeer2000 <57035872+AdamBeer2000@users.noreply.github.com>
2025-04-10 21:42:11 +0200

    Improve test script
    Add test message on fail
    suppress std out on sub process
    fix ranges in test case generation
    add result size check to the test

--- tests/TestOutput.py
+++ tests/TestOutput.py
@@ -15,7 +15,7 @@ def BuildRelease():
     print("Build project finished")
 
 def RunSator(input_dictionary,output,word_lenght,thread_num):
-    subprocess.call(["./build/satorrotas",f"-i{input_dictionary}",f"-o{output}",f"-l{word_lenght}",f"-t{thread_num}"])
+    subprocess.call(["./build/satorrotas",f"-i{input_dictionary}",f"-o{output}",f"-l{word_lenght}",f"-t{thread_num}"],stdout=subprocess.DEVNULL)
 
 def IsPalindrom(matrix):
     size=len(matrix)
@@ -27,12 +27,14 @@ def IsPalindrom(matrix):
 
 def GenerateTestCases():
     TestCases=[]
-    for word_lenght in range(2, 18): 
-        for thread_num in range(1, 12): 
+    for word_lenght in range(2, 19): 
+        for thread_num in range(2, 13): 
             TestCases.append((word_lenght,thread_num))
     return TestCases
 
+#Check : is all of the results really complies with the constraints
 class TestOutputValidity(unittest.TestCase):
+    expextedResultCount=[0,0,361,15590,1452042,10118682,4628694,94863,80,0,0,0,0,0,0,0,0,0,0]
     def get_path(self,word_lenght,thread_num):
             return f"./results_tmp/result_{word_lenght}_{thread_num}.txt"
     def setUp(self):
@@ -41,26 +43,22 @@ class TestOutputValidity(unittest.TestCase):
         shutil.rmtree("./results_tmp")
     @parameterized.expand(GenerateTestCases())  
     def test_n_lenght_m_thread(self,word_lenght,thread_num):
-        print(f"Runing test for {word_lenght} word lenght with {thread_num} no. of threads")
         path=self.get_path(word_lenght,thread_num)
         RunSator("./dictionaries/latin.txt",path,word_lenght,thread_num)
-        self.assertTrue(TestResult(path))
-
-#Check : is all of the results really complies with the constraints
-def TestResult(path):
-    allValid=True
-    try:
+        wordsMatrix=[]
+        allValid=True
+        linecount=0
         with open(path, 'r') as file:
             for line in file:
+                linecount+=1
                 rotasSquare=line.strip()
                 wordsMatrix=rotasSquare.split(" ")
                 if(IsPalindrom(wordsMatrix) is False):
                     allValid=False
-                    print("Invalid matrix : ",wordsMatrix)
                     break
-    except:
-        return False
-    return allValid
+        
+        self.assertTrue(allValid,f'Invalid matrix in the results: word lenght of {word_lenght} and {thread_num} no. of threads Matrix : ({wordsMatrix})') 
+        self.assertTrue(linecount==self.expextedResultCount[word_lenght],f' in the results: word lenght of {word_lenght} and {thread_num} no. of threads Results count: ({linecount}!={self.expextedResultCount[word_lenght]})')            
 
 def CleanUp():
     shutil.rmtree("results_tmp")
